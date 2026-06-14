236466ef06b2ad981e5017e4c0932008adc345df
EdmondFanah <edmondfanah@github.com>
2026-06-14 21:09:28 +0200

    Revert to plain DataGrid namespace, v1.0.7 - clean import

--- package.js
+++ package.js
@@ -20,8 +20,8 @@ const outDir       = path.join(root, 'out', 'controls');
 const zipFile      = path.join(root, 'DataGridPCFSolution.zip');
 const unpackedDir  = path.join(root, 'SolutionUnpacked');
 const otherDir     = path.join(unpackedDir, 'Other');
-const controlsDir  = path.join(unpackedDir, 'Controls', 'dgpc_DataGrid.DataGridControl');
-const SOLUTION_VERSION = '1.0.6.0';
+const controlsDir  = path.join(unpackedDir, 'Controls', 'DataGrid.DataGridControl');
+const SOLUTION_VERSION = '1.0.7.0';
 
 // 1. Check build outputs exist
 ['bundle.js', 'ControlManifest.xml'].forEach(file => {
@@ -43,14 +43,7 @@ fs.mkdirSync(controlsDir, { recursive: true });
 fs.copyFileSync(path.join(outDir, 'bundle.js'),          path.join(controlsDir, 'bundle.js'));
 fs.copyFileSync(path.join(outDir, 'ControlManifest.xml'), path.join(controlsDir, 'ControlManifest.xml'));
 
-// 4a. Patch namespace to include publisher prefix (pcf-scripts rejects underscores so we post-process)
-//     EnhancedTable works because Dataverse stores controls as publisherprefix_namespace.constructor
-//     We replicate that by injecting dgpc_ into the namespace here.
-let manifestXml = fs.readFileSync(path.join(controlsDir, 'ControlManifest.xml'), 'utf8');
-manifestXml = manifestXml.replace(' namespace="DataGrid"', ' namespace="dgpc_DataGrid"');
-fs.writeFileSync(path.join(controlsDir, 'ControlManifest.xml'), manifestXml, 'utf8');
-
-// 4. Write solution.xml (bump version)
+// 5. Write solution.xml (bump version)
 const solutionXml = fs.readFileSync(path.join(root, 'SolutionPackage', 'solution.xml'), 'utf8')
   .replace(/<Version>[^<]*<\/Version>/, `<Version>${SOLUTION_VERSION}</Version>`);
 fs.writeFileSync(path.join(otherDir, 'Solution.xml'), solutionXml, 'utf8');
